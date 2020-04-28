c193a2e26f9b891fefa4ecb8804bb4b5a28a30b1
deepanmania <deepan.kumar@aquera.com>
2020-04-28 07:49:59 +0530

    chat box is done

--- src/components/HeaderBar.js
+++ src/components/HeaderBar.js
@@ -1,5 +1,8 @@
-import React from 'react'
-import { Layout, Row, Col, Menu, BackTop, Badge, Divider } from 'antd'
+import React, { useEffect, useState } from 'react'
+import { Layout, Row, Col, Menu, BackTop, Badge, Drawer, Button, Input, Mentions, Divider } from 'antd'
+import axios from 'axios'
+import TimeAgo from 'react-timeago'
+
 import {
   HomeOutlined,
   ConsoleSqlOutlined,
@@ -7,14 +10,41 @@ import {
   UnorderedListOutlined,
   MenuOutlined,
   NotificationOutlined,
-  UserOutlined
-
+  UserOutlined,
+  MessageOutlined,
+  SendOutlined
 } from '@ant-design/icons'
 import Text from 'antd/lib/typography/Text'
 import '../styles/common.css'
+
+const { Option } = Mentions
 const { Header, Sider, Content } = Layout
 function HeaderBar (props) {
-  return (
+  const [users, setUsers] = useState([])
+  const [messageValue, setMessageValue] = useState('')
+  const [messages, setMessages] = useState([])
+  const userName = 'Deepan'
+  useEffect(() => {
+    console.log('here')
+    axios.get('https://5e9007e9fe7f2a00165ef7b1.mockapi.io/users')
+      .then(res => setUsers(res.data))
+  }, [])
+  const [visible, setVisible] = useState(false)
+  const onClose = () => {
+    setVisible(false)
+  }
+  const showDrawer = () => {
+    console.log(users)
+    setVisible(true)
+  }
+  const sendMessage = () => {
+    setMessageValue('')
+    setMessages(messages.concat([{ messageValue, time: +new Date() }]))
+  }
+  const handleChange = (message) => {
+    setMessageValue(message)
+  }
+  return users.length ? (
     <div style={{ backgroundColor: '#f0f1f4' }}>
       <Layout>
         <Header style={{ position: 'fixed', zIndex: 1, width: '100%' }}>
@@ -23,37 +53,92 @@ function HeaderBar (props) {
               <HomeOutlined className="logo" style={{ color: '#fff' }} />
               <span className="header-title" style={{ padding: '1em' }}>&nbsp;Booking</span>
             </Col>
-            <Col span="auto"><Divider type="vertical" style={{
-              margin: '0px 10px',
-              height: '5em'
-            }}/><MenuOutlined /><Divider type="vertical" style={{
-              margin: '0px 10px',
-              height: '5em'
-            }} />
-            </Col>
-
-            <Col span="auto" >
-              <div>
-                <Badge dot >
-                  <NotificationOutlined />
-                </Badge>
-                <Divider type="vertical" style={{
-                  margin: '0px 10px',
-                  height: '5em'
-                }} />
+            <Col span="1" style={{ textAlign: 'center' }}>
+              <div className="divider-left divider-right">
+                <MenuOutlined className="header-menu"/>
               </div>
-
             </Col>
-            <Col span="auto">
-              <UserOutlined />
+            <Col span="1" style={{ textAlign: 'center' }}>
+              <div className="divider-right">
+                <MessageOutlined className="header-menu" onClick={showDrawer}/>
+              </div>
+            </Col>
+            <Col span="1" style={{ textAlign: 'center' }}>
+              <div className="divider-right">
+                <Badge dot >
+                  <NotificationOutlined className="header-menu"/>
+                </Badge>
+              </div>
+            </Col>
+            <Col span="1" style={{ textAlign: 'right' }}>
+              <UserOutlined className="header-menu"/>&nbsp;
               <Text>Admin</Text>
-              <Divider type="vertical" style={{
-                margin: '0px 10px',
-                height: '5em'
-              }} />
             </Col>
           </Row>
         </Header>
+        <Drawer
+          placement="right"
+          width= "310px"
+          closable={false}
+          onClose={onClose}
+          visible={visible}
+          footer={
+            <div>
+              <Row>
+                <Col span="17">
+                  <Mentions name="messageBox" onChange={handleChange} value={messageValue} style={{
+                    width: '100%',
+                    height: '40px',
+                    borderRadius: '4px',
+                    border: 'solid 1px #eae9e9'
+                  }}>
+                    {users.map((user, idx) => {
+                      return (<Option value={user} key={String(idx)}>{user}</Option>)
+                    })}
+                  </Mentions>
+                </Col>
+                <Col span="3"></Col>
+                <Col span="2">
+                  <Button onClick={sendMessage} style={{
+                    width: '40px',
+                    height: '40px',
+                    borderRadius: '4px',
+                    backgroundColor: '#feae2e'
+                  }}>
+                    <SendOutlined style={{
+                      transform: 'rotate(-30deg)',
+                      color: 'white',
+                      fontSize: '25px',
+                      position: 'absolute',
+                      right: '1px',
+                      bottom: '-6px'
+                    }}/>
+                  </Button>
+                </Col>
+              </Row>
+              <div className="message-tile-meta">Use @ for select agent</div>
+            </div>
+          }
+        >
+          <div style={{ position: 'relative', height: 'auto', width: '100%', bottom: '0px' }}>
+            {messages.map((message, idx) => {
+              return (<div className="message-tile" key={idx}>
+                {message.messageValue}
+                <Divider style={{
+                  backgroundColor: '#ccc9c9',
+                  top: '20px'
+                }}/>
+                <Row>
+                  <Col span="15" className="message-tile-meta">{userName}</Col>
+                  <Col span="auto"></Col>
+                  <Col span="8" className="message-tile-meta"><TimeAgo date={message.time} minPeriod = {10} live={true} /></Col>
+                </Row>
+              </div>)
+            }
+            )}
+          </div>
+
+        </Drawer>
         <Layout>
           <Sider width={80} style={{
             overflow: 'auto',
@@ -95,6 +180,8 @@ function HeaderBar (props) {
         </Layout>
       </Layout>
     </div>
+  ) : (
+    <div>Loading...</div>
   )
 }
 
