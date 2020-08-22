aa481044b98f67f8fe39f3273f65ef42b9cc4f26
Clanizon <hrd@clanizon.com>
2020-08-22 14:58:25 +0530

    Back End Host Name to IP

--- src/constants/index.js
+++ src/constants/index.js
@@ -4,24 +4,24 @@ const NAME_SPACE = 'api/'
 const today = new Date().toISOString().split('T')[0]
 const constants = {
   URL: {
-    GET_ADMIN_MESSAGES: 'http://realkeyip.in:8083/api/user/usermessages',
-    ADD_NEW_MESSAGE: 'http://realkeyip.in:8083/api/user/createmessage',
+    GET_ADMIN_MESSAGES: 'http://35.154.38.203:8083/api/user/usermessages',
+    ADD_NEW_MESSAGE: 'http://35.154.38.203:8083/api/user/createmessage',
     GET_TODAY_LEADS:
-      'http://realkeyip.in:8083/api/leads/leadbyagentanddate?date=' + today,
-    GET_ALL_AGENTS: 'http://realkeyip.in:8083//api/user/userbyrole?roleId=2',
-    ADD_NEW_AGENT: 'http://realkeyip.in:8083//api/user/createuser',
-    ADD_NEW_LEAD: 'http://realkeyip.in:8083/api/leads/createlead',
-    GET_LEAD_STATUS_COUNT: 'http://realkeyip.in:8083/api/leads/leadbystatus',
-    ADD_NEW_AUDIT: 'http://realkeyip.in:8083/api/leads/leadaudit',
-    GET_LEAD_BY_AGENT: 'http://realkeyip.in:8083/api/leads/leadbyagent',
-    LOGIN: 'http://realkeyip.in:8083/api/user/login',
-    GET_ASSET_GROUPS: 'http://realkeyip.in:8083/api/asset/listAssetGroup',
-    ADD_IMAGE_TO_ASSET_GROUP:'http://realkeyip.in:8083/api/documents/adddocument',
-    LIST_GROUP_IMAGE:'http://realkeyip.in:8083/api/documents/listdocumentbygroup?groupId=',
-    ADD_NEW_GROUP: 'http://realkeyip.in:8083/api/asset/addgroup',
+      'http://35.154.38.203:8083/api/leads/leadbyagentanddate?date=' + today,
+    GET_ALL_AGENTS: 'http://35.154.38.203:8083//api/user/userbyrole?roleId=2',
+    ADD_NEW_AGENT: 'http://35.154.38.203:8083//api/user/createuser',
+    ADD_NEW_LEAD: 'http://35.154.38.203:8083/api/leads/createlead',
+    GET_LEAD_STATUS_COUNT: 'http://35.154.38.203:8083/api/leads/leadbystatus',
+    ADD_NEW_AUDIT: 'http://35.154.38.203:8083/api/leads/leadaudit',
+    GET_LEAD_BY_AGENT: 'http://35.154.38.203:8083/api/leads/leadbyagent',
+    LOGIN: 'http://35.154.38.203:8083/api/user/login',
+    GET_ASSET_GROUPS: 'http://35.154.38.203:8083/api/asset/listAssetGroup',
+    ADD_IMAGE_TO_ASSET_GROUP:'http://35.154.38.203:8083/api/documents/adddocument',
+    LIST_GROUP_IMAGE:'http://35.154.38.203:8083/api/documents/listdocumentbygroup?groupId=',
+    ADD_NEW_GROUP: 'http://35.154.38.203:8083/api/asset/addgroup',
     GET_ASSET_BY_GROUP_ID:
-      'http://realkeyip.in:8083/api/asset/listAssetByGroupId',
-    ADD_ASSET: 'http://realkeyip.in:8083/api/asset/addAsset'
+      'http://35.154.38.203:8083/api/asset/listAssetByGroupId',
+    ADD_ASSET: 'http://35.154.38.203:8083/api/asset/addAsset'
   },
   DATA: {
     admin_messages: [
